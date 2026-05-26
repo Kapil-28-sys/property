c42e94fcf1215de1754017361aaaae775205aebc
kapil <vaishnavkapil412@gamil.com>
2026-05-26 16:51:56 +0530

    apiiiss

--- src/servicesapi/buyerformapi.js
+++ src/servicesapi/buyerformapi.js
@@ -21,7 +21,7 @@ export async function submitBuyerEnquiry(formData, property) {
 
   try {
     const response = await fetch(
-      "http://property.concentics.com/api/buyer-details-add",
+      "https://propertybackend.concentics.in/api/buyer-details-add",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
